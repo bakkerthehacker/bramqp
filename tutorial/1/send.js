3df7fd25bab5ab8bbf1150473df1c2ad100a04ca
Grant Bakker <bakkerthehacker@gmail.com>
2013-11-18 23:24:27 -0500

    Added code to match the RabbitMQ tutorial 1

---
+++ tutorial/1/send.js
@@ -0,0 +1,37 @@
+var bramqp = require('bramqp');
+var net = require('net');
+var async = require('async');
+
+bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function(error) {
+	if (error) {
+		console.log(error);
+	}
+	var socket = net.connect({
+		port : 5672
+	}, function() {
+		bramqp.initializeSocket(socket, function(error, handle) {
+			async.series([ function(seriesCallback) {
+				handle.openAMQPCommunication('guest', 'guest', true, seriesCallback);
+			}, function(seriesCallback) {
+				handle.queue.declare(1, 'hello');
+				handle.on('queue.declare-ok', function(channel, method, data) {
+					console.log('queue declared');
+					seriesCallback();
+				});
+			}, function(seriesCallback) {
+				handle.basic.publish(1, '', 'hello', true, false, function() {
+					handle.content(1, 'basic', {}, 'Hello World!', seriesCallback);
+				});
+			}, function(seriesCallback) {
+				setTimeout(function() {
+					handle.closeAMQPCommunication(seriesCallback);
+				}, 10 * 1000);
+			}, function(seriesCallback) {
+				handle.socket.end();
+				setImmediate(seriesCallback());
+			} ], function() {
+				console.log('all done');
+			});
+		});
+	});
+});
