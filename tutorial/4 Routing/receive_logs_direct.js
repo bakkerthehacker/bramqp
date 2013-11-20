0283b58b202d3c1095a3d5eb486ad17115261ca3
Grant Bakker <bakkerthehacker@gmail.com>
2013-11-20 15:20:59 -0500

    Fix some tutorials.  Added tutorial 4 code

--- tutorial/4 Routing/receive_logs_direct.js
+++ tutorial/4 Routing/receive_logs_direct.js
@@ -2,6 +2,8 @@ var bramqp = require('bramqp');
 var net = require('net');
 var async = require('async');
 
+var tempQueueName;
+
 bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function(error) {
 	if (error) {
 		console.log(error);
@@ -13,13 +15,32 @@ bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function
 			async.series([ function(seriesCallback) {
 				handle.openAMQPCommunication('guest', 'guest', true, seriesCallback);
 			}, function(seriesCallback) {
-				handle.queue.declare(1, 'hello');
-				handle.once('queue.declare-ok', function(channel, method, data) {
-					console.log('queue declared');
+				handle.exchange.declare(1, 'direct_logs', 'direct');
+				handle.once('exchange.declare-ok', function(channel, method, data) {
+					console.log('exchange declared');
 					seriesCallback();
 				});
 			}, function(seriesCallback) {
-				handle.basic.consume(1, 'hello', null, false, true, false, false, {});
+				handle.queue.declare(1, '', false, false, true, false, false, {});
+				handle.once('queue.declare-ok', function(channel, method, data) {
+					console.log('queue declared');
+					console.log(data);
+					tempQueueName = data.queue;
+					var args = process.argv.splice(2);
+					if (!args.length) {
+						console.log('Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' [info] [warning] [error]');
+						process.exit(1);
+					}
+					async.each(args, function(severity, eachCallback) {
+						handle.queue.bind(1, data.queue, 'direct_logs', severity, false, {});
+						handle.once('queue.bind-ok', function() {
+							console.log('queue ' + tempQueueName + ' bound to direct_logs');
+							eachCallback();
+						});
+					}, seriesCallback);
+				});
+			}, function(seriesCallback) {
+				handle.basic.consume(1, tempQueueName, null, false, true, false, false, {});
 				handle.once('basic.consume-ok', function(channel, method, data) {
 					console.log('consuming from queue');
 					console.log(data);
