f7529340c98c25b3b03303d75a6cb29b4dde3782
Jesse Brick <jbrick91@gmail.com>
2013-11-23 19:18:34 -0500

    added publisher subscriber tutorial

---
+++ tutorial/3 Publish Subscribe/consumer.js
@@ -0,0 +1,74 @@
+var bramqp = require('bramqp');
+var net = require('net');
+var async = require('async');
+
+bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function(error) {
+	if (error) {
+		console.log(error);
+	}
+	var exchangeName = 'logs';
+	var exchangeType = 'fanout';
+	var queueName = "";
+	var socket = net.connect({
+		port : 5672
+	}, function() {
+		bramqp.initializeSocket(socket, function(error, handle) {
+			async.series([ function(seriesCallback) {
+				handle.openAMQPCommunication('guest', 'guest', true, seriesCallback);
+			}, function(seriesCallback) {
+				var passive = false;
+				var durable = false;
+				var autoDelete = true;
+				var internal = false;
+				var noWait = false;
+				var arguments = {};
+				handle.exchange.declare(1, exchangeName, exchangeType, passive, durable, autoDelete, internal, noWait, arguments);
+				handle.on('exchange.declare-ok', function(channel, method, data) {
+					console.log('exchange declared');
+					seriesCallback();
+				});
+			}, function(seriesCallback) {
+
+				handle.queue.declare(1, null, false, false, false, true, false, {});
+				handle.on('queue.declare-ok', function(channel, method, data) {
+					console.log('queue declared');
+					queueName = data.queue;
+					seriesCallback();
+				});
+			}, function(seriesCallback){
+					handle.queue.bind(1, queueName, exchangeName, null, false, {});
+					handle.on('queue.bind-ok', function(channel, method, data) {
+						console.log('queue bound sucessfully');
+						seriesCallback();
+				});
+			}, function(seriesCallback){
+									
+					var consumerTag = null;
+					var noLocal = false;
+					var noAck = true;
+					var exclusive = true;
+					var noWait = false;
+					var arguments = false;
+					
+					handle.basic.consume(1, queueName, noLocal, noAck, exclusive, noWait, arguments, {});
+					handle.on('basic.consume-ok', function(channel, method, data) {
+						console.log('consuming from queue');
+						console.log(data);
+						handle.on('basic.deliver', function(channel, method, data) {
+							console.log('incoming message');
+							console.log(data);
+							handle.once('content', function(channel, className, properties, content) {
+								console.log('got a message:');
+								console.log(content.toString());
+								console.log('with properties:');
+								console.log(properties);
+								seriesCallback();
+							});
+						});
+					});
+			} ], function() {
+				console.log('all done');
+			});
+		});
+	});
+});
---
+++ tutorial/3 Publish Subscribe/producer.js
@@ -0,0 +1,66 @@
+var bramqp = require('bramqp');
+var net = require('net');
+var async = require('async');
+
+bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function(error) {
+	if (error) {
+		console.log(error);
+	}
+	var exchangeName = 'logs';
+	var exchangeType = 'fanout';
+	
+	var socket = net.connect({
+		port : 5672
+	}, function() {
+		bramqp.initializeSocket(socket, function(error, handle) {
+			async.series([ function(seriesCallback) {
+				handle.openAMQPCommunication('guest', 'guest', true, seriesCallback);
+			}, function(seriesCallback) {
+				var passive = false;
+				var durable = false;
+				var autoDelete = true;
+				var internal = false;
+				var noWait = false;
+				var arguments = {};
+				handle.exchange.declare(1, exchangeName, exchangeType, passive, durable, autoDelete, internal, noWait, arguments);
+				handle.on('exchange.declare-ok', function(channel, method, data) {
+					console.log('exchange declared');
+					seriesCallback();
+				});	
+			}, function(seriesCallback) {
+				var publishErrors = true;
+				var routingKey = 'logKey';
+				var queueMessages = false; // not supported by RabbitMQ
+				handle.basic.publish(1, exchangeName, routingKey, publishErrors, queueMessages, function() {
+					var contentType = 'basic';
+					var properties = {};
+					var content = "11-23-2013: some log";
+					handle.content(1, contentType, {}, content, seriesCallback);
+				});
+			}, function(seriesCallback) {
+				handle.on('basic.return', function(replyCode, replyText, exchange, routingKey) {
+					console.log("Message Returned from Server");
+					console.log(replyCode);
+					console.log(replyText);
+					console.log(exchange);
+				});
+				seriesCallback();
+			}, function(seriesCallback){
+				console.log("message published");
+				setTimeout(function() {
+					console.log("close communication");
+					handle.closeAMQPCommunication(seriesCallback);
+				}, 10 * 1000);
+			}, function(seriesCallback) {
+				console.log("socket ended");
+				handle.socket.end();
+				setImmediate(seriesCallback());
+			} ], function(err) {
+				if (err){
+					console.log(err);
+				}
+				console.log('all done');
+			});
+		});
+	});
+});
