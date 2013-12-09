33b693958a68d359763afc4f463bf2a9ea0f84e3
Grant Bakker <bakkerthehacker@gmail.com>
2013-12-08 23:52:24 -0500

    Fixed tutorial 3

--- tutorial/3 Publish Subscribe/consumer.js
+++ tutorial/3 Publish Subscribe/consumer.js
@@ -13,60 +13,63 @@ bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function
 		port : 5672
 	}, function() {
 		bramqp.initializeSocket(socket, function(error, handle) {
-			async.series([ function(seriesCallback) {
-				handle.openAMQPCommunication('guest', 'guest', true, seriesCallback);
-			}, function(seriesCallback) {
-				var passive = false;
-				var durable = false;
-				var autoDelete = true;
-				var internal = false;
-				var noWait = false;
-				var arguments = {};
-				handle.exchange.declare(1, exchangeName, exchangeType, passive, durable, autoDelete, internal, noWait, arguments);
-				handle.on('exchange.declare-ok', function(channel, method, data) {
-					console.log('exchange declared');
-					seriesCallback();
-				});
-			}, function(seriesCallback) {
+			async.series([
+					function(seriesCallback) {
+						handle.openAMQPCommunication('guest', 'guest', true, seriesCallback);
+					},
+					function(seriesCallback) {
+						var passive = false;
+						var durable = false;
+						var autoDelete = true;
+						var internal = false;
+						var noWait = false;
+						var arguments = {};
+						handle.exchange.declare(1, exchangeName, exchangeType, passive, durable, autoDelete, internal,
+								noWait, arguments);
+						handle.once('exchange.declare-ok', function(channel, method, data) {
+							console.log('exchange declared');
+							seriesCallback();
+						});
+					}, function(seriesCallback) {
 
-				handle.queue.declare(1, null, false, false, false, true, false, {});
-				handle.on('queue.declare-ok', function(channel, method, data) {
-					console.log('queue declared');
-					queueName = data.queue;
-					seriesCallback();
-				});
-			}, function(seriesCallback){
-					handle.queue.bind(1, queueName, exchangeName, null, false, {});
-					handle.on('queue.bind-ok', function(channel, method, data) {
-						console.log('queue bound sucessfully');
-						seriesCallback();
-				});
-			}, function(seriesCallback){
-									
-					var consumerTag = null;
-					var noLocal = false;
-					var noAck = true;
-					var exclusive = true;
-					var noWait = false;
-					var arguments = false;
-					
-					handle.basic.consume(1, queueName, noLocal, noAck, exclusive, noWait, arguments, {});
-					handle.on('basic.consume-ok', function(channel, method, data) {
-						console.log('consuming from queue');
-						console.log(data);
-						handle.on('basic.deliver', function(channel, method, data) {
-							console.log('incoming message');
+						handle.queue.declare(1, null, false, false, false, true, false, {});
+						handle.once('queue.declare-ok', function(channel, method, data) {
+							console.log('queue declared');
+							queueName = data.queue;
+							seriesCallback();
+						});
+					}, function(seriesCallback) {
+						handle.queue.bind(1, queueName, exchangeName, null, false, {});
+						handle.once('queue.bind-ok', function(channel, method, data) {
+							console.log('queue bound sucessfully');
+							seriesCallback();
+						});
+					}, function(seriesCallback) {
+
+						var consumerTag = null;
+						var noLocal = false;
+						var noAck = true;
+						var exclusive = true;
+						var noWait = false;
+						var arguments = false;
+
+						handle.basic.consume(1, queueName, noLocal, noAck, exclusive, noWait, arguments, {});
+						handle.once('basic.consume-ok', function(channel, method, data) {
+							console.log('consuming from queue');
 							console.log(data);
-							handle.once('content', function(channel, className, properties, content) {
-								console.log('got a message:');
-								console.log(content.toString());
-								console.log('with properties:');
-								console.log(properties);
-								seriesCallback();
+							handle.on('basic.deliver', function(channel, method, data) {
+								console.log('incoming message');
+								console.log(data);
+								handle.once('content', function(channel, className, properties, content) {
+									console.log('got a message:');
+									console.log(content.toString());
+									console.log('with properties:');
+									console.log(properties);
+									seriesCallback();
+								});
 							});
 						});
-					});
-			} ], function() {
+					} ], function() {
 				console.log('all done');
 			});
 		});
