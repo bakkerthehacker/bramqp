951d60c015fbad0a7e37e1365fa76e35c9bd754e
Grant Bakker <grant@bakker.pw>
2014-02-27 17:59:53 -0800

    Added amqp version to spec test

--- test/specification-test.js
+++ test/specification-test.js
@@ -11,7 +11,7 @@ vows
 		.describe('specification')
 		.addBatch(
 				{
-					'The rabbitmq/full/amqp0-9-1.stripped.extended specification' : {
+					'The RabbitMQ 0-9-1-extended specification' : {
 						topic : function() {
 							var self = this;
 							specification.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function() {
@@ -27,6 +27,16 @@ vows
 							'should match the specification source' : function(path) {
 								var sourcePath = 'rabbitmq/full/amqp0-9-1.stripped.extended';
 								assert.strictEqual(path, sourcePath);
+							}
+						},
+						'amqp version' : {
+							topic : function(spec) {
+								return spec.amqp
+							},
+							'should be 0-9-1' : function(version) {
+								assert.strictEqual(version.major, '0');
+								assert.strictEqual(version.minor, '9');
+								assert.strictEqual(version.revision, '1');
 							}
 						}
 					}
