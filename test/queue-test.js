4f20b5d75742b7d5c04c108fb74ef712a8dddb39
Grant Bakker <grant@bakker.pw>
2014-03-06 20:30:32 -0800

    Start of queue test

---
+++ test/queue-test.js
@@ -0,0 +1,52 @@
+var vows = require('vows');
+var assert = require('assert');
+var net = require('net');
+var async = require('async');
+
+var bramqp = require('../lib/bramqp');
+
+var puts = require('vows').console.puts({
+	stream : process.stdout
+});
+
+vows
+		.describe('queue')
+		.addBatch(
+				{
+					'A queue' : {
+						topic : function(callback){
+							var self = this;
+							bramqp.selectSpecification('rabbitmq/full/amqp0-9-1.stripped.extended', function(error) {
+								if(error){
+									return self.callback(error);
+								}
+								var socket = net.connect({
+									port : 5672
+								}, function() {
+									bramqp.initializeSocket(socket, function(error, handle) {
+										if(error){
+											return self.callback(error);
+										}
+										async.series([ function(seriesCallback) {
+											handle.openAMQPCommunication(seriesCallback);
+										}, function(seriesCallback) {
+											handle.queue.declare(1, 'test-queue', function(error){
+												if(error){
+													return seriesCallback(error);
+												}
+												handle.once('queue.declare-ok', function(channel, method, data) {
+													seriesCallback();
+												});
+											});
+										} ], function(error) {
+											self.callback(error, handle);
+										});
+									});
+								});
+							});
+						},
+						'should recieve messages sent to it' : function(handle){
+							assert(handle);
+						}
+					}
+				}).export(module);
