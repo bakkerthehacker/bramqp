d75854b2fa5d2a308f1fc1726d724c998e8f212c
Grant Bakker <grant@bakker.pw>
2015-07-06 19:53:10 -0400

    Cleaned up tests

--- test/concurrent-connection-test.js
+++ test/concurrent-connection-test.js
@@ -5,64 +5,48 @@ var vows = require('vows');
 var assert = require('assert');
 
 var net = require('net');
-var specification = require('../lib/specification');
-var ConnectionHandle = require('../lib/connectionHandle');
+var bramqp = require('../lib/bramqp');
 
 var puts = require('vows').console.puts({
 	stream : process.stdout
 });
 
-var connectAMQP = function() {
-	return function () {
-		var self = this;
-		var socket = net.connect({host: 'localhost', port: 5672});
-		socket.once('connect', function() {
-			var spec = 'rabbitmq/full/amqp0-9-1.stripped.extended';
-			var handle = new ConnectionHandle(socket, spec);
+var connectAMQP = function () {
+	var self = this;
+	var socket = net.connect({host: 'localhost', port: 5672});
 
-			var bail = setTimeout(function() {
-				self.callback(false, handle);
-			}, 5000);
-
-			handle.once('init', function() {
-				handle.openAMQPCommunication('guest', 'guest', true,
-					function() {
-						clearTimeout(bail);
-						self.callback(true, handle);
-					});
-			});
+	bramqp.initialize(socket, 'rabbitmq/full/amqp0-9-1.stripped.extended', function(error, handle){
+		handle.openAMQPCommunication('guest', 'guest', true, function() {
+			self.callback(null, handle);
 		});
-	};
+	});
 };
 
-var connectVerify = function() {
-	return function(success, handle, error) {
-		assert.strictEqual(success, true);
-		handle.closeAMQPCommunication(function() {
-			handle.socket.end();
-		});
-	};
+var connectVerify = function(handle) {
+	handle.closeAMQPCommunication(function() {
+		handle.socket.end();
+	});
 };
 
 vows.describe('concurrent connections').addBatch({
 	'concurrent connection 1' : {
-		topic : connectAMQP(),
-		'should be connected' : connectVerify(),
+		topic : connectAMQP,
+		'should be connected' : connectVerify,
 	},
 	'concurrent connection 2' : {
-		topic : connectAMQP(),
-		'should be connected' : connectVerify(),
+		topic : connectAMQP,
+		'should be connected' : connectVerify,
 	},
 	'concurrent connection 3' : {
-		topic : connectAMQP(),
-		'should be connected' : connectVerify(),
+		topic : connectAMQP,
+		'should be connected' : connectVerify,
 	},
 	'concurrent connection 4' : {
-		topic : connectAMQP(),
-		'should be connected' : connectVerify(),
+		topic : connectAMQP,
+		'should be connected' : connectVerify,
 	},
 	'concurrent connection 5' : {
-		topic : connectAMQP(),
-		'should be connected' : connectVerify(),
+		topic : connectAMQP,
+		'should be connected' : connectVerify,
 	}
 }).export(module);
